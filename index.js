7fd65f70a7e67251b8c64b96e26d7481a18ba686
Rahman Elsirafy <rahman2408@gmail.com>
2021-09-24 01:58:11 -0400

    commit tree 2fdb0e9d9964e88071993e5940577e796b8e1dea

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+function mapToNegativize(arry) {
+  let newArry = []
+    for (let i = 0; i < arry.length; i++) {
+      newArry.push(arry[i] * -1);
+    }
+    return newArry;
+}
+
+function mapToNoChange(arry) {
+  let newArry = arry 
+  return newArry;
+}
+
+function mapToDouble(arry) {
+  let newArry = []
+    for (let i = 0; i < arry.length; i++) {
+      newArry.push(arry[i] * 2);
+    }
+    return newArry;
+}
+
+function mapToSquare(arry) {
+  let newArry = []
+    for (let i = 0; i < arry.length; i++) {
+      newArry.push(arry[i] ** 2);
+    }
+    return newArry;
+}
+
+function reduceToTotal(arry, start = 0) {
+  let finValue = start
+   
+  for (let i = 0; i < arry.length; i++) {
+    finValue = finValue + arry[i];
+  }
+  return finValue;
+}
+const isTrue = (num) => !!num 
+
+function reduceToAllTrue(arry) {
+   if (arry.every(isTrue)) {return true}
+   else {return false}
+} 
+function reduceToAnyTrue(arry) {
+  if (arry.some(isTrue)) {return true}
+  else {return false}
+} 
